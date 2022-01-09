48c75096456343dc15a2c3fb56b6e83de3247cc4
Algirdas <algirdas.arlauskas1@gmail.com>
2022-01-09 20:20:07 +0200

    finishing course section 3 part

--- src/test/java/com/herokuapp/theinternet/PositiveTests.java
+++ src/test/java/com/herokuapp/theinternet/PositiveTests.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class PositiveTests {
@@ -21,10 +22,10 @@ public class PositiveTests {
         System.out.println("Page is opened");
 
         //sleep for 3 seconds
-        sleep(3000);
+        //sleep(3000);
 
         //maximize browser window
-        driver.manage().window().maximize();
+        //driver.manage().window().maximize();
 
         //enter username
         WebElement username = driver.findElement(By.id("username"));
@@ -40,14 +41,22 @@ public class PositiveTests {
 
         //verifications:
         //  new url
-        sleep(3000);
+        //sleep(3000);
+        String expectedURL = "http://the-internet.herokuapp.com/secure";
+        String actualURL = driver.getCurrentUrl();
+        Assert.assertEquals(actualURL, expectedURL, "Actual page url is not the same as expected");
 
         //  logout button is visible
         WebElement logoutButton = driver.findElement(By.xpath("//div[@id='content']//a[@href='/logout']/i[@class='icon-2x icon-signout']"));
+        Assert.assertTrue(logoutButton.isDisplayed(), "The logout button is not visible");
 
         //  successful login message
-        WebElement successfulMessage = driver.findElement(By.cssSelector("div#flash"));
-
+        WebElement successfulMessage = driver.findElement(By.xpath("/html//div[@id='flash']"));
+        String expectedMessage = "You logged into a secure area!";
+        String actualMessage = successfulMessage.getText();
+        //Assert.assertEquals(actualMessage, expectedMessage, "Actual message is not correct");
+        Assert.assertTrue(actualMessage.contains(expectedMessage),"Actual message is not correct. \nActual Message: " + actualMessage + "\nExpected message: " + expectedMessage);
+        
         //close browser
         driver.quit();
     }
